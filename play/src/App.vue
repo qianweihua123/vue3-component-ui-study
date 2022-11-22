d7a18752d6feed52de02e3fdc2c76acf033053eb
qwh <15806293089@163.com>
2022-11-22 17:00:13 +0800

    feat: 测试提交 lint-staged

--- play/src/App.vue
+++ play/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: qwh 15806293089@163.com
  * @Date: 2022-11-17 14:48:47
  * @LastEditors: qwh 15806293089@163.com
- * @LastEditTime: 2022-11-22 16:33:54
+ * @LastEditTime: 2022-11-22 16:59:18
  * @FilePath: /vue3-component-ui/play/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
